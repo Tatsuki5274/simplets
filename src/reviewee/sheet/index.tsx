259db2de0d95abc7aa3a1f54d4f24b6c7b5793b3
stwatanabe <67622192+stwatanabe@users.noreply.github.com>
2020-08-31 14:04:42 +0900

    ルーティングの実装 (#3)

---
+++ src/reviewee/sheet/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {BrowserRouter, Route, Link, Switch } from "react-router-dom";
+
+function RevieweeSheetShow() {
+  return (
+      <div>未実装の画面</div>
+  );
+}
+
+export default RevieweeSheetShow;
