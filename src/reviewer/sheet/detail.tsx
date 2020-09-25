8ffc8c0b717ecd21e52a698b7a57c18fe4e982e1
yhamazaki <69029845+yhamazaki@users.noreply.github.com>
2020-09-25 15:28:25 +0900

    目標情報の表示 (#42)
    * 評価画面　情報取得コンポーネント　追加
    
    * 今後のキャリア計画　本人希望　追加
    
    * 評価画面　情報取得コンポーネント　追加
    
    * 今後のキャリア計画　本人希望　追加
    
    * 評価画面　インタビュー記録日付　コンポーネント追加
    
    * インタビュー実施記録　コンポーネント修正
    
    * インタビュー実施記録　情報取得コンポーネント　修正
    
    * 本人希望　コンポーネント修正
    ビジネス成果目標　コンポーネント追加
    
    * 本人希望　修正
    
    * 本人希望　コンポーネント　修正
    
    * インタビュー実施　メモ追加
    
    * 本人希望　修正
    
    * 評価画面　情報取得コンポーネント　追加
    
    * 今後のキャリア計画　本人希望　追加
    
    * 評価画面　インタビュー記録日付　コンポーネント追加
    
    * インタビュー実施記録　コンポーネント修正
    
    * インタビュー実施記録　情報取得コンポーネント　修正
    
    * 本人希望　コンポーネント修正
    ビジネス成果目標　コンポーネント追加
    
    * 本人希望　修正
    
    * 本人希望　コンポーネント　修正
    
    * インタビュー実施　メモ追加
    
    * 本人希望　修正
    
    * ビジネス成果目標　情報取得コンポーネント　修正
    
    * ビジネス成果目標　取得情報表示
    
    * ビジネス姿勢目標　成長目標　情報表示　追加
    
    * 目標情報　表示修正
    
    * 目標コンポーネント　修正
    
    * 不要なコンポーネント　削除
    
    * 不要コンポーネント　削除
    
    * 情報取得コンポーネント　修正
    
    * 不要コンポーネント　削除
    
    * ビジネス・成長目標　コンポーネント修正
    
    * 目標コンポーネント　カテゴリ分岐　追加
    
    * 目標コンポーネント　修正
    
    * インポート情報　修正
    
    * 目標コンポーネント　追加
    
    * 情報取得コンポーネント　修正
    
    * インタビュー実施記録　情報表示　追加
    
    * コメント　修正
    
    * objective 参照方法 修正
    
    * objective 情報取得 追加
    
    * 情報取得 コンポーネント 修正
    
    * シートがnullの場合のエラー文言 追加
    
    * 評価画面の遷移先を変更
    
    * 取得情報 コンソール表示 変更
    
    * パラメータ変更 修正
    
    * インデント 修正
    sheetId 修正
    
    * sheetId 修正
    
    * sheet変数 修正
    
    * 設定目標 表示方法 変更
    
    * ビジネス成果目標 分岐追加
    
    * カテゴリid分岐 修正
    
    * fixしたカテゴリ分岐 追加
    
    * カテゴリ分岐 int型へ修正
    
    * カテゴリ分岐 修正
    目標コンポーネント表示完了
    
    * 目標コンポーネント 優先順位 追加
    
    * 目標一覧 一括表示 追加
    
    * 不要コンポーネント 削除
    
    * 不要コンポーネント 削除
    
    * 不要なコード 削除

--- src/reviewer/sheet/detail.tsx
+++ src/reviewer/sheet/detail.tsx
@@ -34,7 +34,7 @@ function EvalutionScreen(props: Props) {
     useEffect(() => {
         ; (async () => {
             //const sheetId = props.match.params.sheetId;
-
+          
             try {
                 const input: APIt.GetSheetQueryVariables = {
                     id: sheetId
@@ -189,202 +189,70 @@ function EvalutionScreen(props: Props) {
                             </Form.Group>
                         </Form><br />
 
-                        {/* ビジネス成果目標 */}
-                        <h4>ビジネス成果目標</h4>
-                        <Table bordered itemID="business">
-                            <thead>
-                                <tr>
-                                    <td>目標</td>
-                                    <td>実績</td>
-                                    <td>ステータス</td>
-                                    <td>自己評価</td>
-                                    <td>最終評価</td>
-                                    <td>更新日時</td>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>A社でのOJTを着実に実施し、有償稼働できる状況にする。</td>
-                                    <td>A社での保守作業に１０月から有償で参加することができた</td>
-                                    <td>実施完了</td>
-                                    <td>4</td>
-                                    <td>
-                                        <InputGroup>
-                                            <DropdownButton title="select">
-                                                <Dropdown.Item value="5">5</Dropdown.Item>
-                                                <Dropdown.Item value="4">4</Dropdown.Item>
-                                                <Dropdown.Item value="3">3</Dropdown.Item>
-                                                <Dropdown.Item value="2">2</Dropdown.Item>
-                                                <Dropdown.Item value="1">1</Dropdown.Item>
-                                            </DropdownButton>
-                                        </InputGroup>
-                                    </td>
-                                    <td>2020/08/01 14:33</td>
-                                </tr>
-                                <tr>
-                                    <td>A社SAP運用保守に有償で参加し、与えられた仕事に対して指導を受けながら成果を出す。</td>
-                                    <td></td>
-                                    <td>未着手</td>
-                                    <td></td>
-                                    <td>
-                                        <InputGroup>
-                                            <DropdownButton title="select">
-                                                <Dropdown.Item value="5">5</Dropdown.Item>
-                                                <Dropdown.Item value="4">4</Dropdown.Item>
-                                                <Dropdown.Item value="3">3</Dropdown.Item>
-                                                <Dropdown.Item value="2">2</Dropdown.Item>
-                                                <Dropdown.Item value="1">1</Dropdown.Item>
-                                            </DropdownButton>
-                                        </InputGroup>
-                                    </td>
-                                    <td>2020/08/02 15:20</td>
-                                </tr>
-                                <tr>
-                                    <td>A社担当チームの一員としてお客様X様空合格点をもらう</td>
-                                    <td></td>
-                                    <td>実施中</td>
-                                    <td></td>
-                                    <td>
-                                        <InputGroup>
-                                            <DropdownButton title="select">
-                                                <Dropdown.Item value="5">5</Dropdown.Item>
-                                                <Dropdown.Item value="4">4</Dropdown.Item>
-                                                <Dropdown.Item value="3">3</Dropdown.Item>
-                                                <Dropdown.Item value="2">2</Dropdown.Item>
-                                                <Dropdown.Item value="1">1</Dropdown.Item>
-                                            </DropdownButton>
-                                        </InputGroup>
-                                    </td>
-                                    <td>2020/08/01 14:20</td>
-                                </tr>
-                            </tbody>
-                        </Table><br />
+                        {/* 目標コンポーネント */}
+                        {sheet.section?.items?.map((arg: any) => {
+                            const section: Section = arg    //仮の型変換処理
+                            return (
+                                <div key={section.id}>
+                                    <h4>{section.category?.name}</h4>
+                                    <Table bordered>
+                                        <thead>
+                                            <tr>
+                                                <td>目標</td>
+                                                <td>実績</td>
+                                                <td>ステータス</td>
+                                                <td>優先順位</td>
+                                                <td>自己評価</td>
+                                                <td>最終評価</td>
+                                                <td>更新日時</td>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {section.objective?.items?.map((arg: any) => {
+                                                const objective: Objective = arg;   //仮の型変換処理
+                                                const date = new Date(objective.updatedOn);
+                                                return (
+                                                    <tr key={objective.id}>
 
-                        {/* ビジネス姿勢目標 */}
-                        <h4>ビジネス姿勢目標</h4>
-                        <Table bordered>
-                            <thead>
-                                <tr>
-                                    <td>目標</td>
-                                    <td>実績</td>
-                                    <td>ステータス</td>
-                                    <td>自己評価</td>
-                                    <td>最終評価</td>
-                                    <td>更新日時</td>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>所属長へのOJT週報を徹底する　OJT期間だけでなく年間を通して報告する</td>
-                                    <td>１１月まではほぼ実施できたが１２月はあまり報告できなかった。業務多忙により１２月は報告できなかったが報告内容は適切であった。</td>
-                                    <td>実施完了</td>
-                                    <td>3</td>
-                                    <td>
-                                        <InputGroup>
-                                            <DropdownButton title="select">
-                                                <Dropdown.Item value="5">5</Dropdown.Item>
-                                                <Dropdown.Item value="4">4</Dropdown.Item>
-                                                <Dropdown.Item value="3">3</Dropdown.Item>
-                                                <Dropdown.Item value="2">2</Dropdown.Item>
-                                                <Dropdown.Item value="1">1</Dropdown.Item>
-                                            </DropdownButton>
-                                        </InputGroup>
-                                    </td>
-                                    <td>2020/08/01 14:33</td>
-                                </tr>
-                                <tr>
-                                    <td>部門会議にてプロジェクト報告を実施する。　年度末までに一回。</td>
-                                    <td>２月の部門会議でA社に関するプロジェクト報告を実施した</td>
-                                    <td>実施完了</td>
-                                    <td>3</td>
-                                    <td>
-                                        <InputGroup>
-                                            <DropdownButton title="select">
-                                                <Dropdown.Item value="5">5</Dropdown.Item>
-                                                <Dropdown.Item value="4">4</Dropdown.Item>
-                                                <Dropdown.Item value="3">3</Dropdown.Item>
-                                                <Dropdown.Item value="2">2</Dropdown.Item>
-                                                <Dropdown.Item value="1">1</Dropdown.Item>
-                                            </DropdownButton>
-                                        </InputGroup>
-                                    </td>
-                                    <td>2020/08/02 15:20</td>
-                                </tr>
-                                <tr>
-                                    <td>イベントや勉強会に積極的に参加し、自分を売り込む</td>
-                                    <td>イベントや勉強会には毎回参加できた。</td>
-                                    <td>実施完了</td>
-                                    <td>3</td>
-                                    <td>
-                                        <InputGroup>
-                                            <DropdownButton title="select">
-                                                <Dropdown.Item value="5">5</Dropdown.Item>
-                                                <Dropdown.Item value="4">4</Dropdown.Item>
-                                                <Dropdown.Item value="3">3</Dropdown.Item>
-                                                <Dropdown.Item value="2">2</Dropdown.Item>
-                                                <Dropdown.Item value="1">1</Dropdown.Item>
-                                            </DropdownButton>
-                                        </InputGroup>
-                                    </td>
-                                    <td>2020/08/01 14:20</td>
-                                </tr>
-                            </tbody>
-                        </Table><br />
+                                                        {/* 目標本文 */}
+                                                        <td>{objective.content}</td>
 
-                        {/* 成長目標 */}
-                        <h4>成長目標</h4>
-                        <Table bordered>
-                            <thead>
-                                <tr>
-                                    <td>目標</td>
-                                    <td>実績</td>
-                                    <td>ステータス</td>
-                                    <td>自己評価</td>
-                                    <td>最終評価</td>
-                                    <td>更新日時</td>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>人人研修に出席し、SEとしての基礎スキルを身についける。
-解らないことはそのままにせず、自分で調べたり他人に聞いて解決するように心がける。</td>
-                                    <td>新人研修には積極的に取り組み、問題なく終了することができた</td>
-                                    <td>実施完了</td>
-                                    <td>4</td>
-                                    <td>
-                                        <InputGroup>
-                                            <DropdownButton title="select">
-                                                <Dropdown.Item value="5">5</Dropdown.Item>
-                                                <Dropdown.Item value="4">4</Dropdown.Item>
-                                                <Dropdown.Item value="3">3</Dropdown.Item>
-                                                <Dropdown.Item value="2">2</Dropdown.Item>
-                                                <Dropdown.Item value="1">1</Dropdown.Item>
-                                            </DropdownButton>
-                                        </InputGroup>
-                                    </td>
-                                    <td>2020/08/01 14:33</td>
-                                </tr>
-                                <tr>
-                                    <td>仕事を通じて様々なことを吸収し、社会人として成長する</td>
-                                    <td>A社A社SAP運用保守に１０月から有償で参加し、指導を受けながら活動した。不明な点は自分で調査した上で相手に確認する等、期待通りの成果をあげた。
-積極的な仕事への取り組みはお客様からも評価されている。</td>
-                                    <td>実施完了</td>
-                                    <td>3</td>
-                                    <td>
-                                        <InputGroup>
-                                            <DropdownButton title="select">
-                                                <Dropdown.Item value="5">5</Dropdown.Item>
-                                                <Dropdown.Item value="4">4</Dropdown.Item>
-                                                <Dropdown.Item value="3">3</Dropdown.Item>
-                                                <Dropdown.Item value="2">2</Dropdown.Item>
-                                                <Dropdown.Item value="1">1</Dropdown.Item>
-                                            </DropdownButton>
-                                        </InputGroup>
-                                    </td>
-                                    <td>2020/08/02 15:20</td>
-                                </tr>
-                            </tbody>
-                        </Table>
+                                                        {/* 実績 */}
+                                                        <td>{objective.result}</td>
+
+                                                        {/* ステータス文字列 */}
+                                                        <td>{objective.status}</td>
+
+                                                        {/* 優先順位 */}
+                                                        <td>{objective.priority}</td>
+
+                                                        {/* 自己評価 */}
+                                                        <td>{objective.selfEvaluation}</td>
+
+                                                        {/* 最終評価 */}
+                                                        <td>
+                                                            {/* 仮フォーム機能 後ほど修正*/}
+                                                            <InputGroup>
+                                                                <DropdownButton title="select">
+                                                                    <Dropdown.Item value="5">5</Dropdown.Item>
+                                                                    <Dropdown.Item value="4">4</Dropdown.Item>
+                                                                    <Dropdown.Item value="3">3</Dropdown.Item>
+                                                                    <Dropdown.Item value="2">2</Dropdown.Item>
+                                                                    <Dropdown.Item value="1">1</Dropdown.Item>
+                                                                </DropdownButton>
+                                                            </InputGroup>
+                                                        </td>
+
+                                                        {/* 更新日時 */}
+                                                        <td>{dateFormat(date, "yyyy/mm/dd HH:MM")}</td>
+                                                    </tr>
+                                                )
+                                            })}
+                                        </tbody>
+                                    </Table>
+                                </div>
+                            )
+                        })}
 
                     </div>
 
