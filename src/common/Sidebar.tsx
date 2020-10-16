b13730c90afa98f79ed5b2d2dc85a3d20b93af6d
yhamazaki <69029845+yhamazaki@users.noreply.github.com>
2020-10-16 18:14:20 +0900

    進捗参照ボタンの出し分けを実装 (#106)
    * cognitoユーザのグループ情報を取得
    進捗参照のリンクの表示分岐を追加
    
    * 不要なコメントの削除
    
    * 進捗参照ボタンの表示方法を変更
    
    * 不要なコメントアウトの削除
    進捗参照ボタンの表示方法の変更

--- src/common/Sidebar.tsx
+++ src/common/Sidebar.tsx
@@ -1,17 +1,53 @@
-import React from 'react';
+import { Auth } from 'aws-amplify';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import style from './sidebarStyle.module.scss'
 
 function SidebarComponents() {
-    // const sidebarDocked = true;
-    // // const sidebarOpen = false; 
-    // // const sidebarTransitions = true;
+
+    //ユーザ情報取得
+    //const groups: string[] = ["test","test2"];
+
+    const groups: string[] = [""];
+    const [isManager, setIsManager] = useState<boolean>();
+    useEffect(() => {
+        ; (async () => {
+            try {
+                const currentUser = await Auth.currentAuthenticatedUser()
+                console.log("currentUser", currentUser)
+                const currentUserGroups = currentUser.signInUserSession.idToken.payload["cognito:groups"];
+
+                for (let i = 0; i < currentUserGroups.length; i++) {
+                    groups[i] = currentUserGroups[i];
+                    console.log(groups[i]);
+                }
+                console.log(groups);
+
+
+                // admin,managerが含まれているか確認
+                for (let i = 0; i < groups.length; i++) {
+                    if (groups[i].indexOf('Admin') >= 0 || groups[i].indexOf('Manager') >= 0) {
+                        setIsManager(true);
+
+                    }
+                }
+
+            } catch (e) {
+                console.log(e);
+            }
+
+        })()
+    }, []);
+
     return (
         // サイドバーを表示
         <div>
             <Link className={style.sidebarContents} to="/reviewee/list">業績評価一覧</Link><br />
-            <Link className={style.sidebarContents} to="/reviewer/list">進捗参照</Link>
-        </div>        
+
+            {/* 進捗参照 表示条件 */}
+            {isManager? <Link className={style.sidebarContents} to="/reviewer/list">進捗参照</Link>: ""}
+
+        </div>
     );
 }
 
