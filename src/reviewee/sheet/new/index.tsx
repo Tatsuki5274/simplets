32db598d3b4a19ba89a34e7ec4192e1f43d0c68b
yhamazaki <69029845+yhamazaki@users.noreply.github.com>
2020-10-29 14:20:27 +0900

    目標登録画面　Formikの導入 (#166)
    * Formikタグの追加
    
    * Formikの導入
    
    * 不要な機能のコメントアウト

--- src/reviewee/sheet/new/index.tsx
+++ src/reviewee/sheet/new/index.tsx
@@ -11,6 +11,7 @@ import CategoryInput from "./categoryInput"
 import { createObjective } from 'graphql/mutations';
 import { Redirect } from 'react-router-dom';
 import HeaderComponents from 'common/header';
+import { Formik } from 'formik';
 
 type Props = {
     match: {
@@ -33,43 +34,43 @@ function RevieweeSheetNew(props: Props){
 
     const [isRedirect, setIsRedirect] = useState<boolean>();
     const [sections, setSections] = useState<Section[]>();
-    const [input, setInput] = useState<InputForm>({});
+    // const [input, setInput] = useState<InputForm>({});
 
-    function handleChange(event: ChangeEvent<HTMLInputElement>){
-        const target = event.target;
-        const value = target.type === 'checkbox' ? target.checked : target.value;
-        const name = target.name;
-        setInput({ ...input, [name]: value });
-    }
+    // function handleChange(event: ChangeEvent<HTMLInputElement>){
+    //     const target = event.target;
+    //     const value = target.type === 'checkbox' ? target.checked : target.value;
+    //     const name = target.name;
+    //     setInput({ ...input, [name]: value });
+    // }
 
-    async function onSubmit(event: ChangeEvent<HTMLInputElement>){
-        event.preventDefault();
-        const createI: APIt.CreateObjectiveInput = {
-            content: input.content || "",
-            priority: input.priority || "",
-            objectiveSectionId: input.section || "",
-            expStartDate: input.expStartDate?.replace('T', '-') || "",
-            expDoneDate: input.expDoneDate?.replace('T', '-') || "",
-        }
-        const createMV: APIt.CreateObjectiveMutationVariables = {
-            input: createI
-        }
-        let createR: GraphQLResult<APIt.CreateObjectiveMutation>
-        try{
-            createR = await API.graphql(graphqlOperation(createObjective, createMV)) as GraphQLResult<APIt.CreateObjectiveMutation>;
-        }catch(e){
-            console.log("エラーを無視しています", e)
-            console.log("データが不完全でないことを確認してください")
-            createR = e;
-        }
-        if(createR.data){
-            const createTM: APIt.CreateObjectiveMutation = createR.data;
-            setIsRedirect(true);
-        }else{
-            console.log("保存に失敗しました")
-        }
+    // async function onSubmit(event: ChangeEvent<HTMLInputElement>){
+    //     event.preventDefault();
+    //     const createI: APIt.CreateObjectiveInput = {
+    //         content: input.content || "",
+    //         priority: input.priority || "",
+    //         objectiveSectionId: input.section || "",
+    //         expStartDate: input.expStartDate?.replace('T', '-') || "",
+    //         expDoneDate: input.expDoneDate?.replace('T', '-') || "",
+    //     }
+    //     const createMV: APIt.CreateObjectiveMutationVariables = {
+    //         input: createI
+    //     }
+    //     let createR: GraphQLResult<APIt.CreateObjectiveMutation>
+    //     try{
+    //         createR = await API.graphql(graphqlOperation(createObjective, createMV)) as GraphQLResult<APIt.CreateObjectiveMutation>;
+    //     }catch(e){
+    //         console.log("エラーを無視しています", e)
+    //         console.log("データが不完全でないことを確認してください")
+    //         createR = e;
+    //     }
+    //     if(createR.data){
+    //         const createTM: APIt.CreateObjectiveMutation = createR.data;
+    //         setIsRedirect(true);
+    //     }else{
+    //         console.log("保存に失敗しました")
+    //     }
 
-    }
+    // }
 
     useEffect(() => {
         ;(async()=>{
@@ -105,50 +106,91 @@ function RevieweeSheetNew(props: Props){
             <HeaderComponents />
             
             <Container>
-                <Form onSubmit={onSubmit}>
-                    <div>
-                        {sections?.map((section: Section)=>{
-                            if(section.category && section.category.name){
-                                return <CategoryInput key={section.id} handleChange={handleChange} sectionId={section.id} categoryName={section.category?.name}></CategoryInput>
-                            }else{
-                                console.log("エラー: カテゴリが設定されていない可能性があります。")
-                            }
-                        })}
-                    </div>
-                    <Form.Label>目標内容</Form.Label>
-                    <Form.Control onChange={handleChange} name="content" />
+                <Formik
+                    initialValues={{
+                        section: '',
+                        content: '',
+                        priority: '',
+                        expStartDate: '',
+                        expDoneDate: '',
 
-                    <Form.Label>優先順位</Form.Label>
-                    <Form.Control as="select" onChange={handleChange} name="priority">
-                        <option></option>
-                        <option>A</option>
-                        <option>B</option>
-                        <option>C</option>
-                    </Form.Control>
-                    <Row>
-                        <Col md="2" lg="2" xl="2">開始予定日</Col>
-                        <Col md="4" lg="4" xl="4">
-                            <Form.Control
-                                required
-                                type="date"
-                                name="expStartDate"
-                                onChange={handleChange}
-                            />
-                        </Col>
-                        <Col md="2" lg="2" xl="2">完了予定日</Col>
-                        <Col md="4" lg="4" xl="4">
-                            <Form.Control
-                                required
-                                type="date"
-                                name="expDoneDate"
-                                onChange={handleChange}
-                            />
-                        </Col>
-                    </Row>
-                    <Button variant="success" type="submit">目標登録</Button>{' '}
-                </Form>
+                    }}
+                    onSubmit={async (values, actions) => {
+                        console.log('values', values);
 
+                        const createI: APIt.CreateObjectiveInput = {
+                            content: values.content || "",
+                            priority: values.priority || "",
+                            objectiveSectionId: values.section || "",
+                            expStartDate: values.expStartDate?.replace('T', '-') || "",
+                            expDoneDate: values.expDoneDate?.replace('T', '-') || "",
+                        }
+                        console.log('createI',createI);
+                        const createMV: APIt.CreateObjectiveMutationVariables = {
+                            input: createI
+                        }
+                        let createR: GraphQLResult<APIt.CreateObjectiveMutation>
+                        try {
+                            createR = await API.graphql(graphqlOperation(createObjective, createMV)) as GraphQLResult<APIt.CreateObjectiveMutation>;
+                        } catch (e) {
+                            console.log("エラーを無視しています", e)
+                            console.log("データが不完全でないことを確認してください")
+                            createR = e;
+                        }
+                        if (createR.data) {
+                            const createTM: APIt.CreateObjectiveMutation = createR.data;
+                            setIsRedirect(true);
+                        } else {
+                            console.log("保存に失敗しました")
+                        }
 
+                    }}
+                >
+                    {props => (
+                        <form onSubmit={props.handleSubmit}>
+                            <div>
+                                {sections?.map((section: Section) => {
+                                    if (section.category && section.category.name) {
+                                        return <CategoryInput key={section.id} handleChange={props.handleChange} sectionId={section.id} categoryName={section.category?.name}></CategoryInput>
+                                    } else {
+                                        console.log("エラー: カテゴリが設定されていない可能性があります。")
+                                    }
+                                })}
+                            </div>
+                            <Form.Label>目標内容</Form.Label>
+                            <Form.Control name="content" onChange={props.handleChange}/>
+
+                            <Form.Label>優先順位</Form.Label>
+                            <Form.Control as="select" name="priority" onChange={props.handleChange}>
+                                <option></option>
+                                <option>A</option>
+                                <option>B</option>
+                                <option>C</option>
+                            </Form.Control>
+                            <Row>
+                                <Col md="2" lg="2" xl="2">開始予定日</Col>
+                                <Col md="4" lg="4" xl="4">
+                                    <Form.Control
+                                        required
+                                        type="date"
+                                        name="expStartDate"
+                                        onChange={props.handleChange}
+                                    />
+                                </Col>
+                                <Col md="2" lg="2" xl="2">完了予定日</Col>
+                                <Col md="4" lg="4" xl="4">
+                                    <Form.Control
+                                        required
+                                        type="date"
+                                        name="expDoneDate"
+                                        onChange={props.handleChange}
+                                    />
+                                </Col>
+                            </Row>
+                            <Button variant="success" type="submit">目標登録</Button>{' '}
+                        </form>
+                    )}
+                </Formik>
             </Container>
         </div>
     )
