042c0e3c785b50eb1fbdbd01e7eea7e78f3c99f7
stwatanabe <67622192+stwatanabe@users.noreply.github.com>
2020-11-05 11:36:11 +0900

    ゲージの実装 (#183)
    * 平均算出処理の分離
    
    * 画面表示の完全分離
    
    * インデント調整
    
    * 古い処理の削除
    
    * コメントを追加
    
    * ゲージ表示の実装
    
    * 二階層平均の実装
    
    * 全体平均のゲージ表示
    
    * ゲージの色を変更
    
    * 不要な記述を削除

--- src/reviewer/list/progress.tsx
+++ src/reviewer/list/progress.tsx
@@ -6,12 +6,28 @@ import { listGroups, listSheets } from 'graphql/queries';
 import { GetEmployeeQuery, ListGroupsQuery, ListSheetsQuery } from 'API';
 import { GraphQLResult } from "@aws-amplify/api";
 import * as APIt from 'API';
-import { Group, Objective, Section, Sheet } from 'App';
+import { Employee, Group, Objective, Section, Sheet } from 'App';
 import { getEmployee } from 'graphql/queries'
 import SidebarComponents from 'common/Sidebar';
 import HeaderComponents from 'common/header';//ヘッダーの表示
 import style from './progressStyle.module.scss';
 import { Link } from 'react-router-dom';
+import { ArcGauge } from '@progress/kendo-react-gauges';
+
+type ViewType = {
+    sheetId: string
+    reviewee: {
+        lastName: string,
+        firstName: string
+    }
+    groupName: string
+    categorys: {
+        name: string | undefined
+        avg: number
+    }[] | undefined,
+    avg: number
+}
+
 
 function ProgressReferenceList() {
     //今日の日付を取得
@@ -23,6 +39,8 @@ function ProgressReferenceList() {
     }
 
     const [sheets, setSheets] = useState<Sheet[]>();
+    const [sheetsView, setSheetsView] = useState<ViewType[]>();
+    
 
     const [groupValue, setGroupValue] = useState([]);
     const handleChangeBottun = (val: any) => setGroupValue(val);
@@ -72,6 +90,7 @@ function ProgressReferenceList() {
 
     //選択された部門と年度によって各社員の進捗情報を抽出する
     async function handleClickSearch() {
+        let listItems: Sheet[];
         //部門の選択肢の中から「全て」が選択された場合
         if (formInput.groupId === "all") {
             const listQV: APIt.ListSheetsQueryVariables = { filter: { year: { eq: parseInt(formInput.year as string) } } };
@@ -82,7 +101,7 @@ function ProgressReferenceList() {
                 console.log("エラーを無視しています", e);
                 response = e;
             }
-            const listItems = response.data?.listSheets?.items;
+            listItems = response.data?.listSheets?.items;
             setSheets(listItems as Sheet[]);
             console.log(response);
             console.log(listItems);
@@ -98,16 +117,96 @@ function ProgressReferenceList() {
                 console.log("エラーを無視しています", e);
                 response = e;
             }
-            const listItems = response.data?.listSheets?.items;
+            listItems = response.data?.listSheets?.items;
             setSheets(listItems as Sheet[]);
             console.log(response);
             console.log(listItems);
         }
+
+        // 平均値を算出する処理
+        const getAgvObjective = (section: Section)=>{
+            let sum = 0;
+            let cnt = 0;
+            let ret = -1;
+            section?.objective?.items?.forEach((objective)=>{
+                if(objective && objective.progress){
+                    sum += objective?.progress;
+                    cnt++;
+                }
+            })
+            if(cnt > 0) ret = sum / cnt;
+            return ret;
+        }
+        const getAvg = (nums: number[]) =>{
+            let sum = 0;
+            let cnt = 0;
+            let ret = -1;
+
+            nums.forEach((num)=>{
+                if(num !== -1){
+                    sum += num;
+                    cnt++;
+                }
+            })
+            if(cnt > 0) ret = sum / cnt;
+            return ret;
+        }
+        // 画面表示に必要な情報を加工する処理
+        let viewTemp: ViewType[] = listItems.map((sheet: Sheet)=>{
+            return {
+                sheetId: sheet.id,
+                groupName: sheet.group?.name || "",
+                reviewee: {
+                    firstName: sheet.revieweeEmployee?.firstName || "",
+                    lastName: sheet.revieweeEmployee?.lastName || ""
+                },
+                categorys: sheet.section?.items?.map((section)=>{
+                    return {
+                        name: section?.category?.name,
+                        avg: getAgvObjective(section as Section)
+                    }
+                }),
+                avg: -1
+            }
+        })
+        const view = viewTemp.map(item=>{
+            return {
+                ...item,
+                avg: item.categorys ? 
+                    getAvg(item.categorys.map(cat=>{
+                        return cat.avg;
+                    })) 
+                : -1
+            }            
+        })
+
+        setSheetsView(view);
     }
 
-    return (
+    const arcColors = [
+        {
+            to: 25,
+            color: '#0058e9'
+        }, {
+            from: 25,
+            to: 50,
+            color: '#f31700'
+        }, {
+            from: 50,
+            to: 75,
+            color: '#ffc000'
+        }, {
+            to: 75,
+            color: '#00ffff'
+        }, {
+            to: 100,
+            color: '#7fff00'
+        }
+    ];
 
+    return (
         <div>
+            <div id="gage"></div>
             {/* ヘッダーの表示 */}
             <HeaderComponents />
             {/* サイドバーコンポーネント 表示 */}
@@ -154,46 +253,49 @@ function ProgressReferenceList() {
 
                 </Form>
                 <br />
-                {/* 取得したシートの数だけ社員毎の進捗を表示 */}
-                {sheets?.map((sheet: Sheet) => {
-                    return (
+
+                {sheetsView?.map(view => {
+                   return (
                         <Card className={style.linkbox}>
-                            <Link to={`/reviewer/sheet/${sheet.id}`} />
+                            <Link to={`/reviewer/sheet/${view.sheetId}`} />
                             {/* 社員の姓名と部門名を表示 */}
-                            <Card.Header>{sheet.revieweeEmployee?.lastName}{sheet.revieweeEmployee?.firstName}&nbsp;{sheet.group?.name}</Card.Header>
+                           <Card.Header>
+                               {view.reviewee.lastName}
+                               {view.reviewee.firstName}
+                               &nbsp;
+                               {view.groupName}
+                               &nbsp;
+                               {view.avg !== -1 ? `${view.avg}%` : null}
+                               <ArcGauge
+                                    {...{
+                                        value: view.avg,
+                                        colors: arcColors
+                                    }} style={{
+                                        width: '50px',
+                                        height: '50px',
+                                        display: 'inline-block'
+                                    }} 
+                                />
+                            </Card.Header>
                             <Card.Body>
-                                {/* セクション毎に進捗の平均を表示 */}
-                                {sheet.section?.items?.map((arg: any) => {
-                                    const section: Section = arg    //仮の型変換処理
-                                    let sum = 0;
-                                    {/* セクション内の全オブジェクトの進捗を合計 */ }
-                                    section.objective?.items?.forEach((objective: any) => {
-                                        sum += objective.progress;
-                                    })
-                                    let avg = 0;
-                                    {/* オブジェクトの進捗の平均を表示 */ }
-                                    if (section && section.objective && section.objective.items) {
-                                        avg = Math.floor((sum / section.objective.items.length) as number);
-                                    }
-                                    {/* データがないcategoryの進捗の表示分け */ }
-                                    if (isNaN(avg)) {
-                                        return (
-                                            <div>
-                                                <td>{section.category?.name}&nbsp;</td><td>-</td><br />
-                                            </div>
-                                        )
-                                    } else {
-                                        return (
-                                            <div>
-                                                <td>{section.category?.name}&nbsp;</td><td>{avg}%</td><br />
-                                            </div>
-                                        )
-                                    }
+                                {view.categorys?.map(category=>{
+                                    return <div>
+                                        {category.name}&nbsp;{category.avg === -1 ? "-" : category.avg}
+                                        <ArcGauge {...{
+                                            value: category.avg,
+                                            colors: arcColors
+                                        }} style={{
+                                            width: '50px',
+                                            height: '50px',
+                                            display: 'inline-block'
+                                        }} />
+                                    </div>
                                 })}
                             </Card.Body>
                         </Card>
                     )
                 })}
+
 
             </Container>
         </div>
