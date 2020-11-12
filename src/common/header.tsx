2376851162fd3bc7be5cd67cbbc998e58142b5c9
yhamazaki <69029845+yhamazaki@users.noreply.github.com>
2020-11-12 16:26:44 +0900

    ヘッダーを固定表示に変更 (#225)
    * Navbarへ変更
    
    * classNameを修正
    
    * 不要な宣言を削除

--- src/common/header.tsx
+++ src/common/header.tsx
@@ -6,7 +6,7 @@ import { getEmployee } from 'graphql/queries';
 import { GetEmployeeQuery } from 'API';
 import { GraphQLResult } from "@aws-amplify/api";
 import style from './headerStyle.module.scss'
-import { Button } from 'react-bootstrap';
+import { Button, Navbar } from 'react-bootstrap';
 
 
 
@@ -53,14 +53,16 @@ function HeaderComponents() {
 
     //ヘッダー表示
     return (
-      <header className={style.headerColorContents}> 
-        <div className={style.headerRight}>
-            <p className={style.headerContents}>{employee?.company?.name}</p>
-            <p className={style.headerContents}>{employee?.group?.name}</p>
-            <p className={style.headerContents}>{employee?.lastName}{employee?.firstName}</p>
-            <Button onClick={signOut}>ログアウト</Button>
-        </div>
-      </header>
+        <Navbar bg="dark" variant="dark" sticky="top">
+            <Navbar.Brand>業績評価システム</Navbar.Brand>
+            <Navbar.Toggle />
+            <Navbar.Collapse className="justify-content-end">
+                <Navbar.Text className={style.headerContents}>{employee?.company?.name}</Navbar.Text>
+                <Navbar.Text className={style.headerContents}>{employee?.group?.name}</Navbar.Text>
+                <Navbar.Text className={style.headerContents}>{employee?.lastName}{employee?.firstName}</Navbar.Text>
+                <Button onClick={signOut}>ログアウト</Button>
+            </Navbar.Collapse>
+        </Navbar>
     );
 }
 
