cd6a0e6adb21171e641cf4c42a6ea42c653ac215
stwatanabe <67622192+stwatanabe@users.noreply.github.com>
2020-11-17 16:01:41 +0900

    カテゴリが表示されない不具合に対処 (#237)

--- src/reviewee/sheet/new/index.tsx
+++ src/reviewee/sheet/new/index.tsx
@@ -2,9 +2,9 @@ import React, { useEffect, useState } from 'react';
 import { Container, Row, Col, Button, Form, Badge } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { API, Auth, graphqlOperation } from 'aws-amplify';
-import { Employee, Section } from 'App';
+import { Employee, Section, Sheet } from 'App';
 import { GraphQLResult } from "@aws-amplify/api";
-import { getEmployee, listSections } from 'graphql/queries';
+import { getEmployee, getSheet, listSections } from 'graphql/queries';
 import * as APIt from 'API';
 import { ListSectionsQuery, ListSectionsQueryVariables, ModelSectionFilterInput } from 'API';
 import CategoryInput from "./categoryInput"
@@ -13,6 +13,7 @@ import { Redirect } from 'react-router-dom';
 import HeaderComponents from 'common/header';
 import { ErrorMessage, Formik } from 'formik';
 import * as Yup from 'yup';
+import { SheetDao } from 'lib/dao/sheetDao';
 
 type Props = {
     match: {
@@ -32,9 +33,9 @@ type InputForm = {
 
 function RevieweeSheetNew(props: Props){
     const sheetId = props.match.params.sheetId;
+    const [sheet, setSheet] = useState<Sheet | null>(null)
 
     const [isRedirect, setIsRedirect] = useState<boolean>();
-    const [sections, setSections] = useState<Section[]>();
 
     const [companyGroups, setCompanyGroups] = useState<Array<string>>();
 
@@ -75,158 +76,169 @@ function RevieweeSheetNew(props: Props){
 
     useEffect(() => {
         ;(async()=>{
-
-            const queryInput: ListSectionsQueryVariables = {
-                filter:{
-                    sectionSheetId: {
-                        eq: sheetId
+            const sheet = await SheetDao.get(getSheet, {id: sheetId})
+            if(sheet && sheet.section && sheet.section.items){
+                sheet.section.items.sort(function (a, b) {
+                    if (a?.category?.no! > b?.category?.no!) {
+                        return 1;
+                    } else {
+                        return -1;
                     }
-                } as ModelSectionFilterInput
+                });
+                setSheet(sheet)
             }
-            const response = (await API.graphql(graphqlOperation(listSections, queryInput))
-            )as GraphQLResult<ListSectionsQuery>;
-            const repsonseSection = response.data?.listSections?.items as Section[];
-            
-            //カテゴリ情報のnoを元にソート
-            repsonseSection?.sort(function (a, b) {
-                if (a.category?.no! > b.category?.no!) {
-                    return 1;
-                } else {
-                    return -1;
-                }
-            });
-            setSections(repsonseSection);
 
-            // companyGroupsを取得
+
+            // const queryInput: ListSectionsQueryVariables = {
+            //     filter:{
+            //         sectionSheetId: {
+            //             eq: sheetId
+            //         }
+            //     } as ModelSectionFilterInput
+            // }
+            // const response = (await API.graphql(graphqlOperation(listSections, queryInput))
+            // )as GraphQLResult<ListSectionsQuery>;
+            // const repsonseSection = response.data?.listSections?.items as Section[];
+            
+            // //カテゴリ情報のnoを元にソート
+            // repsonseSection?.sort(function (a, b) {
+            //     if (a.category?.no! > b.category?.no!) {
+            //         return 1;
+            //     } else {
+            //         return -1;
+            //     }
+            // });
+            // setSections(repsonseSection);
+
+            // // companyGroupsを取得
             getCompanyGroup();
+            // console.log(repsonseSection)
         })()
       },[sheetId]);
       
     if(isRedirect) return <Redirect to={`/reviewee/sheet/${sheetId}`} />
-    return (
-        <div>
-            {/* ヘッダーの表示 */}
-            <HeaderComponents />
-            
-            <Container>
-                <Formik
-                    initialValues={{
-                        section: '',
-                        content: '',
-                        priority: '',
-                        expStartDate: '',
-                        expDoneDate: '',
-                    }}
-                    validationSchema={Yup.object({
-                        expStartDate: Yup.date().typeError('正しく入力してください').required('必須入力です'),
-                        expDoneDate: Yup.date().min(Yup.ref('expStartDate'), ({ min }) => `開始予定日より後の日付を入力してください`,)
-                            .typeError('正しく入力してください')
-                            .required('必須入力です'),
-                        section: Yup.string().required('目標カテゴリを選択してください'),
-                        priority: Yup.string().required('必須入力です'),
-                        content: Yup.string().required('必須入力です')
-                    })}
-                    
-                    onSubmit={async (values, actions) => {
-                        console.log('values', values);
+    else if(sheet){
+        return (
+            <div>
+                {/* ヘッダーの表示 */}
+                <HeaderComponents />
+                
+                <Container>
+                    <Formik
+                        initialValues={{
+                            section: '',
+                            content: '',
+                            priority: '',
+                            expStartDate: '',
+                            expDoneDate: '',
+                        }}
+                        validationSchema={Yup.object({
+                            expStartDate: Yup.date().typeError('正しく入力してください').required('必須入力です'),
+                            expDoneDate: Yup.date().min(Yup.ref('expStartDate'), ({ min }) => `開始予定日より後の日付を入力してください`,)
+                                .typeError('正しく入力してください')
+                                .required('必須入力です'),
+                            section: Yup.string().required('目標カテゴリを選択してください'),
+                            priority: Yup.string().required('必須入力です'),
+                            content: Yup.string().required('必須入力です')
+                        })}
+                        
+                        onSubmit={async (values, actions) => {
+                            console.log('values', values);
 
-                        //会社グループ権限が存在する場合,ミューテーション処理を実行
-                        if(companyGroups && companyGroups[0] && companyGroups[1] && companyGroups[2]) {
+                            //会社グループ権限が存在する場合,ミューテーション処理を実行
+                            if(companyGroups && companyGroups[0] && companyGroups[1] && companyGroups[2]) {
 
-                            const createI: APIt.CreateObjectiveInput = {
-                                content: values.content || "",
-                                priority: values.priority || "",
-                                objectiveSectionId: values.section || "",
-                                expStartDate: values.expStartDate?.replace('T', '-') || "",
-                                expDoneDate: values.expDoneDate?.replace('T', '-') || "",
-                                readGroups: [companyGroups[0]],
-                                updateGroups: [companyGroups[1], companyGroups[2]],
-                                progress: 0
-                            }
-                            console.log('createI',createI);
-                            const createMV: APIt.CreateObjectiveMutationVariables = {
-                                input: createI
-                            }
-                            let createR: GraphQLResult<APIt.CreateObjectiveMutation>
-                            try {
-                                createR = await API.graphql(graphqlOperation(createObjective, createMV)) as GraphQLResult<APIt.CreateObjectiveMutation>;
-                            } catch (e) {
-                                console.log("エラーを無視しています", e)
-                                console.log("データが不完全でないことを確認してください")
-                                createR = e;
-                            }
-                            if (createR.data) {
-                                const createTM: APIt.CreateObjectiveMutation = createR.data;
-                                setIsRedirect(true);
+                                const createI: APIt.CreateObjectiveInput = {
+                                    content: values.content || "",
+                                    priority: values.priority || "",
+                                    objectiveSectionId: values.section || "",
+                                    expStartDate: values.expStartDate?.replace('T', '-') || "",
+                                    expDoneDate: values.expDoneDate?.replace('T', '-') || "",
+                                    readGroups: [companyGroups[0]],
+                                    updateGroups: [companyGroups[1], companyGroups[2]],
+                                    progress: 0
+                                }
+                                console.log('createI',createI);
+                                const createMV: APIt.CreateObjectiveMutationVariables = {
+                                    input: createI
+                                }
+                                let createR: GraphQLResult<APIt.CreateObjectiveMutation>
+                                try {
+                                    createR = await API.graphql(graphqlOperation(createObjective, createMV)) as GraphQLResult<APIt.CreateObjectiveMutation>;
+                                } catch (e) {
+                                    console.log("エラーを無視しています", e)
+                                    console.log("データが不完全でないことを確認してください")
+                                    createR = e;
+                                }
+                                if (createR.data) {
+                                    const createTM: APIt.CreateObjectiveMutation = createR.data;
+                                    setIsRedirect(true);
+                                } else {
+                                    console.log("保存に失敗しました")
+                                }
                             } else {
-                                console.log("保存に失敗しました")
+                                console.error("会社グループの取得に失敗しました",companyGroups);
                             }
-                        } else {
-                            console.error("会社グループの取得に失敗しました",companyGroups);
-                        }
 
-                    }}
-                >
-                    {props => (
-                        <form onSubmit={props.handleSubmit}>
-                            <div>
-                                {sections?.map((section: Section) => {
-                                    if (section.category && section.category.name) {
-                                        return <div>
-                                            <CategoryInput
-                                                key={section.id}
-                                                handleChange={props.handleChange}
-                                                sectionId={section.id}
-                                                categoryName={section.category?.name}
-                                            ></CategoryInput>
-                                        </div>
-                                    } else {
-                                        console.log("エラー: カテゴリが設定されていない可能性があります。")
-                                    }
-                                })}
-                                <p><ErrorMessage name="section" /></p>
-                            </div>
-                            <Form.Label>目標内容</Form.Label>
-                            <Form.Control as="textarea" name="content" onChange={props.handleChange}/>
-                            <p><ErrorMessage name="content" /></p>
+                        }}
+                    >
+                        {props => (
+                            <form onSubmit={props.handleSubmit}>
+                                <div>
+                                    {sheet.section?.items?.map((section) => {
+                                        if (section && section.category && section.category.name) {
+                                            return <CategoryInput key={section.id} handleChange={props.handleChange} sectionId={section.id} categoryName={section.category?.name}></CategoryInput>
+                                        } else {
+                                            console.log("エラー: カテゴリが設定されていない可能性があります。")
+                                        }
+                                    })}
+                                    <p><ErrorMessage name="section" /></p>
+                                </div>
+                                <Form.Label>目標内容</Form.Label>
+                                <Form.Control as="textarea" name="content" onChange={props.handleChange}/>
+                                <p><ErrorMessage name="content" /></p>
 
-                            <Form.Label>優先順位</Form.Label>
-                            <Form.Control as="select" name="priority" onChange={props.handleChange}>
-                                <option></option>
-                                <option>A</option>
-                                <option>B</option>
-                                <option>C</option>
-                            </Form.Control>
-                            <p><ErrorMessage name="priority" /></p>
-                            <Row>
-                                <Col md="2" lg="2" xl="2">開始予定日<Badge variant="danger">必須</Badge></Col>
-                                <Col md="4" lg="4" xl="4">
-                                    <Form.Control
-                                        //required
-                                        type="date"
-                                        name="expStartDate"
-                                        onChange={props.handleChange}
-                                    />
-                                    <ErrorMessage name="expStartDate" />
-                                </Col>
-                                <Col md="2" lg="2" xl="2">完了予定日<Badge variant="danger">必須</Badge></Col>
-                                <Col md="4" lg="4" xl="4">
-                                    <Form.Control
-                                        //required
-                                        type="date"
-                                        name="expDoneDate"
-                                        onChange={props.handleChange}
-                                    />
-                                    <ErrorMessage name="expDoneDate" />
-                                </Col>
-                            </Row>
-                            <Button variant="success" type="submit">目標登録</Button>{' '}
-                        </form>
-                    )}
-                </Formik>
-            </Container>
-        </div>
-    )
+                                <Form.Label>優先順位</Form.Label>
+                                <Form.Control as="select" name="priority" onChange={props.handleChange}>
+                                    <option></option>
+                                    <option>A</option>
+                                    <option>B</option>
+                                    <option>C</option>
+                                </Form.Control>
+                                <p><ErrorMessage name="priority" /></p>
+                                <Row>
+                                    <Col md="2" lg="2" xl="2">開始予定日<Badge variant="danger">必須</Badge></Col>
+                                    <Col md="4" lg="4" xl="4">
+                                        <Form.Control
+                                            //required
+                                            type="date"
+                                            name="expStartDate"
+                                            onChange={props.handleChange}
+                                        />
+                                        <ErrorMessage name="expStartDate" />
+                                    </Col>
+                                    <Col md="2" lg="2" xl="2">完了予定日<Badge variant="danger">必須</Badge></Col>
+                                    <Col md="4" lg="4" xl="4">
+                                        <Form.Control
+                                            //required
+                                            type="date"
+                                            name="expDoneDate"
+                                            onChange={props.handleChange}
+                                        />
+                                        <ErrorMessage name="expDoneDate" />
+                                    </Col>
+                                </Row>
+                                <Button variant="success" type="submit">目標登録</Button>{' '}
+                            </form>
+                        )}
+                    </Formik>
+                </Container>
+            </div>
+        )
+    }else{
+        console.error("sheetが存在しません")
+        return <span>表示にエラーが生じました</span>
+    }
 }
 
 export default RevieweeSheetNew;
