9fc744a3f0f029bec23530b6f2cd2bfc637b6e77
yhamazaki <69029845+yhamazaki@users.noreply.github.com>
2020-11-26 16:50:17 +0900

    日付のフォーマットを修正 (#272)

--- src/lib/awsdate.ts
+++ src/lib/awsdate.ts
@@ -1,5 +1,5 @@
 import dateFormat from "dateformat";
 
 export const formatAWSDate = (date: Date): string =>{
-    return dateFormat(date, "yyyy-mm-dd-hh:MM");
+    return dateFormat(date, "yyyy-mm-dd-HH:MM");
 }
