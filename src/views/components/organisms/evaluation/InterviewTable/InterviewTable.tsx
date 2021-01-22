8945c3932b75d24e9640650ab6eb0017225ed4d9
stwatanabe <67622192+stwatanabe@users.noreply.github.com>
2021-01-22 16:30:21 +0900

    日付の形式を修正 (#427)
    * 日付の形式を修正
    
    * 形式を変更

--- src/views/components/organisms/evaluation/InterviewTable/InterviewTable.tsx
+++ src/views/components/organisms/evaluation/InterviewTable/InterviewTable.tsx
@@ -1,3 +1,4 @@
+import dateFormat from "dateformat";
 import React from "react";
 import { Table } from "react-bootstrap";
 import TableBodyCell from "views/components/molecules/TableBodyCell";
@@ -15,6 +16,24 @@ type Props = {
 }
 
 export default function(props: Props){
+    const format = "yyyy/mm/dd"
+    let interviewPlanDate: string | null = null
+    if(props.interviewPlanDate){
+        interviewPlanDate = dateFormat(props.interviewPlanDate, format)
+    } 
+    let InterviewMid1Date: string | null = null
+    if(props.InterviewMid1Date){
+        InterviewMid1Date = dateFormat(props.InterviewMid1Date, format)
+    }
+    let InterviewMid2Date: string | null = null
+    if(props.InterviewMid2Date){
+        InterviewMid2Date = dateFormat(props.InterviewMid2Date, format)
+    }
+    let InterviewMid3Date: string | null = null
+    if(props.InterviewMid3Date){
+        InterviewMid3Date = dateFormat(props.InterviewMid3Date, format)
+    }
+    
     return (
         <Table bordered hover>
             <tr>
@@ -24,22 +43,22 @@ export default function(props: Props){
             </tr>
             <tr>
                 <TableBodyCell>目標設定</TableBodyCell>
-                <TableBodyCell>{props.interviewPlanDate || ""}</TableBodyCell>
+                <TableBodyCell>{interviewPlanDate || ""}</TableBodyCell>
                 <TableBodyCell>{props.interviewPlanComment || ""}</TableBodyCell>
             </tr>
             <tr>
                 <TableBodyCell>中間#1</TableBodyCell>
-                <TableBodyCell>{props.InterviewMid1Date || ""}</TableBodyCell>
+                <TableBodyCell>{InterviewMid1Date || ""}</TableBodyCell>
                 <TableBodyCell>{props.InterviewMid1Comment || ""}</TableBodyCell>
             </tr>
             <tr>
                 <TableBodyCell>中間#2</TableBodyCell>
-                <TableBodyCell>{props.InterviewMid2Date || ""}</TableBodyCell>
+                <TableBodyCell>{InterviewMid2Date || ""}</TableBodyCell>
                 <TableBodyCell>{props.InterviewMid2Comment || ""}</TableBodyCell>
             </tr>
             <tr>
                 <TableBodyCell>中間#3</TableBodyCell>
-                <TableBodyCell>{props.InterviewMid3Date || ""}</TableBodyCell>
+                <TableBodyCell>{InterviewMid3Date || ""}</TableBodyCell>
                 <TableBodyCell>{props.InterviewMid3Comment || ""}</TableBodyCell>
             </tr>
         </Table>
