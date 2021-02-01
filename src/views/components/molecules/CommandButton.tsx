eedf950231f13440bcf0c18421c8dd605b756f00
yhamazaki <69029845+yhamazaki@users.noreply.github.com>
2021-02-01 15:43:25 +0900

    CommandButtonを作成 (#471)

---
+++ src/views/components/molecules/CommandButton.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { ButtonProps } from "react-bootstrap";
+import Button from "../atoms/Button";
+
+type Props = ButtonProps & {
+    children: string
+}
+
+export default function (props: Props) {
+    return (
+        <Button
+            variant="primary"
+            {...props}
+        >
+            {props.children}
+        </Button>
+    )
+}
