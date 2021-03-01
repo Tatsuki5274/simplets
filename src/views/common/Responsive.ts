c8273293e9c32ea76f7ce7cc7c253f9b490e8478
stwatanabe <67622192+stwatanabe@users.noreply.github.com>
2021-03-01 18:02:05 +0900

    レスポンシブオブジェクトの作成 (#579)

---
+++ src/views/common/Responsive.ts
@@ -0,0 +1,15 @@
+export const onMobile = "@media(min-width: 500px)"
+export const onDesktop = "@media(min-width: 1024px)"
+
+// 使い方
+
+// const SectionTitle = styled.div({
+//     fontSize: '25px',
+//     display: 'flex',
+//     [onMobile]: {    // モバイルの時のみ反映
+//         fontSize: '40px'
+//     },
+//     [onDesktop]: {   // デスクトップの時のみ反映
+//         fontSize: '100px'
+//     }
+// })
