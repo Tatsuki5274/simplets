1fbe87ef77f0ab9c2c7c64fe5c48551ff8c36476
yhamazaki <69029845+yhamazaki@users.noreply.github.com>
2021-05-14 13:15:37 +0900

    社員登録画面を修正 (#693)
    * メールアドレスの重複確認を追加
    
    * 社員番号の重複確認を実装
    
    * バリデーションを追加
    
    * 社員番号の重複確認を修正

--- src/views/components/organisms/admin/employee/AdminEmployeeCreateInput.tsx
+++ src/views/components/organisms/admin/employee/AdminEmployeeCreateInput.tsx
@@ -1,18 +1,21 @@
-import { BooleanType, CreateEmployeeInput, EmployeeType } from "API";
+import { BooleanType, CreateEmployeeInput, EmployeeType, GetEmployeeQueryVariables, ListEmployeesCompanyQueryVariables } from "API";
 import { ErrorContext } from "App";
-import { Formik } from "formik";
+import { ErrorMessage, Formik } from "formik";
 import { createEmployee } from "graphql/mutations";
+import { getEmployee, listEmployeesCompany } from "graphql/queries";
 import { EmployeeDao } from "lib/dao/employeeDao";
 import React, { useContext } from "react";
 import { useHistory } from "react-router";
 import { routeBuilder } from "router";
 import styled from "styled-components";
 import Button from "views/components/atoms/Button";
+import ErrorText from "views/components/atoms/ErrorText";
 import Text from "views/components/atoms/Text";
 import TextField from "views/components/atoms/TextField";
 import { SelectLabel } from "views/components/atoms/Types";
 import CommandButton from "views/components/molecules/CommandButton";
 import PullDown from "views/components/molecules/PullDown";
+import * as Yup from 'yup';
 
 type Props = {
     groups: SelectLabel[],
@@ -45,31 +48,69 @@ export default function (props: Props) {
                 isAdmin: props.isAdmin[0].value,
                 manager: props.manager[0].value,
             }}
+            validationSchema={Yup.object({
+                localID: Yup.string().required('社員番号を入力してください'),
+                lastName: Yup.string().required('姓を入力してください'),
+                firstName: Yup.string().required('名を入力してください'),
+                groupList: Yup.string().required('所属部署を選択してください'),
+                email: Yup.string().required('メールアドレスを入力してください'),
+                grade: Yup.string().required('等級を入力してください'),
+                isAdmin: Yup.string().required('マスター管理を選択してください'),
+                manager: Yup.string().required('参照権限を選択してください'),
+            })}
             onSubmit={async (values) => {
                 // console.log("values", values)
-                if (window.confirm("社員登録を行いますか？")) {
-                    const createI: CreateEmployeeInput = {
-                        companyID: props.companyId,
-                        email: values.email,
-                        groupID: values.groupList,
-                        firstName: values.firstName,
-                        grade: values.grade,
-                        isDeleted: BooleanType.FALSE,
-                        lastName: values.lastName,
-                        no: values.localID,
-                        manager: values.manager === "NORMAL" ? EmployeeType.NORMAL : values.manager === "MANAGER" ? EmployeeType.MANAGER : values.manager === "SUPER_MANAGER" ? EmployeeType.SUPER_MANAGER : EmployeeType.OTHER,
-                        superiorUsername: values.superiorList,
-                        username: values.email,
-                        isCompanyAdmin: values.isAdmin === "true" ? true : false,
+
+                // 社員番号の重複確認
+                const listI: ListEmployeesCompanyQueryVariables = {
+                    companyID: props.companyId,
+                    no: {
+                        eq: values.localID
                     }
-                    const createItem = await EmployeeDao.create(createEmployee, createI);
-                    if (createItem) {
-                        window.confirm("社員情報の登録が完了しました。");
-                        history.push(routeBuilder.adminEmployeeListPath());
+                }
+                const listItem = await EmployeeDao.listCompany(listEmployeesCompany, listI);
+                if (listItem) {
+                    if (listItem.length === 0) {
+
+                        // メールアドレスの重複確認
+                        const getI: GetEmployeeQueryVariables = {
+                            username: values.email,
+                        }
+                        const getItem = await EmployeeDao.get(getEmployee, getI);
+                        if (!getItem) {
+                            if (window.confirm("社員登録を行いますか？")) {
+                                const createI: CreateEmployeeInput = {
+                                    companyID: props.companyId,
+                                    email: values.email,
+                                    groupID: values.groupList,
+                                    firstName: values.firstName,
+                                    grade: values.grade,
+                                    isDeleted: BooleanType.FALSE,
+                                    lastName: values.lastName,
+                                    no: values.localID,
+                                    manager: values.manager === "NORMAL" ? EmployeeType.NORMAL : values.manager === "MANAGER" ? EmployeeType.MANAGER : values.manager === "SUPER_MANAGER" ? EmployeeType.SUPER_MANAGER : EmployeeType.OTHER,
+                                    superiorUsername: values.superiorList,
+                                    username: values.email,
+                                    isCompanyAdmin: values.isAdmin === "true" ? true : false,
+                                }
+                                const createItem = await EmployeeDao.create(createEmployee, createI);
+                                if (createItem) {
+                                    window.confirm("社員情報の登録が完了しました。");
+                                    history.push(routeBuilder.adminEmployeeListPath());
+                                } else {
+                                    setError("社員情報の登録に失敗しました")
+                                    console.error("社員情報の登録に失敗しました")
+                                }
+                            }
+                        } else {
+                            window.alert("メールアドレスが既に登録されています。")
+                        }
                     } else {
-                        setError("社員情報の登録に失敗しました")
-                        console.error("社員情報の登録に失敗しました")
+                        window.alert("社員番号が既に登録されています。")
                     }
+                } else {
+                    setError("社員情報の取得に失敗しました")
+                    console.error("社員情報の取得に失敗しました")
                 }
             }}
         >
@@ -85,8 +126,12 @@ export default function (props: Props) {
                                     name="localID"
                                     onChange={formik.handleChange}
                                 />
+                                <ErrorText>
+                                    <ErrorMessage name="localID" />
+                                </ErrorText>
                             </td>
                         </tr>
+
 
                         <tr>
                             <td>
@@ -101,6 +146,12 @@ export default function (props: Props) {
                                     name="firstName"
                                     onChange={formik.handleChange}
                                 />
+                                <ErrorText>
+                                    <ErrorMessage name="lastName" />
+                                </ErrorText>
+                                <ErrorText>
+                                    <ErrorMessage name="firstName" />
+                                </ErrorText>
                             </td>
                         </tr>
 
@@ -114,6 +165,9 @@ export default function (props: Props) {
                                     handleChange={formik.handleChange}
                                     options={props.groups}
                                 />
+                                <ErrorText>
+                                    <ErrorMessage name="groupList" />
+                                </ErrorText>
                             </td>
                         </tr>
 
@@ -126,6 +180,9 @@ export default function (props: Props) {
                                     name="email"
                                     onChange={formik.handleChange}
                                 />
+                                <ErrorText>
+                                    <ErrorMessage name="email" />
+                                </ErrorText>
                             </td>
                         </tr>
 
@@ -138,6 +195,9 @@ export default function (props: Props) {
                                     name="grade"
                                     onChange={formik.handleChange}
                                 />
+                                <ErrorText>
+                                    <ErrorMessage name="grade" />
+                                </ErrorText>
                             </td>
                         </tr>
 
@@ -151,6 +211,9 @@ export default function (props: Props) {
                                     handleChange={formik.handleChange}
                                     options={props.superiors}
                                 />
+                                <ErrorText>
+                                    <ErrorMessage name="superiorList" />
+                                </ErrorText>
                             </td>
                         </tr>
 
@@ -164,6 +227,9 @@ export default function (props: Props) {
                                     handleChange={formik.handleChange}
                                     options={props.isAdmin}
                                 />
+                                <ErrorText>
+                                    <ErrorMessage name="isAdmin" />
+                                </ErrorText>
                             </td>
                         </tr>
 
@@ -177,6 +243,9 @@ export default function (props: Props) {
                                     handleChange={formik.handleChange}
                                     options={props.manager}
                                 />
+                                <ErrorText>
+                                    <ErrorMessage name="manager" />
+                                </ErrorText>
                             </td>
                         </tr>
 
